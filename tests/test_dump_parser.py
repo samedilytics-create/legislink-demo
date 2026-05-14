09b3cff71284d0c2d2ed591dc6ef6c24e2da39b4
Snic9 <snic9004@gmail.com>
2026-05-13 22:23:21 -0600

    test(parser): cover escape edge cases

--- tests/test_dump_parser.py
+++ tests/test_dump_parser.py
@@ -16,3 +16,28 @@ def test_bill_rows_have_expected_columns():
     assert bills[0]["id"] == "1"
     assert bills[0]["bill_number"] == "HB0001"
     assert bills[0]["sponsor"] == "Rep. Test"
+
+
+def test_tab_escape_inside_value():
+    tables = dict(parse_dump(FIXTURE))
+    assert tables["bill"][0]["title"] == "Tab\tin Title"
+
+
+def test_null_marker_becomes_none():
+    tables = dict(parse_dump(FIXTURE))
+    assert tables["bill"][1]["sponsor"] is None
+
+
+def test_backslash_escape():
+    tables = dict(parse_dump(FIXTURE))
+    assert tables["bill"][2]["title"] == "Backslash \\ test"
+
+
+def test_newline_escape_in_value():
+    tables = dict(parse_dump(FIXTURE))
+    assert "two\nlines" in tables["bill"][0]["detailed_description"]
+
+
+def test_quoted_table_name():
+    tables = dict(parse_dump(FIXTURE))
+    assert "user" in tables  # was `public."user"` in dump
